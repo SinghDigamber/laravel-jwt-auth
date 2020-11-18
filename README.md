125505650056da42b97fbc0d1453ce946c52fb50
Digamber Singh <digamber@positronx.io>
2020-11-18 18:39:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# laravel-jwt-auth
+
+[Laravel 7|8 JWT Authentication Tutorial: User Login & Signup API](https://www.positronx.io/laravel-jwt-authentication-tutorial-user-login-signup-api/)
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
